5a1468bef94650227c10888dee15ffd41a0d3cda
LuanHietbrink <53099726+LuanHietbrink@users.noreply.github.com>
2022-11-17 15:17:10 +0200

    Added datahandler for modules

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,14 @@ namespace PRG281_Project_Group11
 {
     public partial class Form1 : Form
     {
+        ModuleDataHandler moduleDataHandler = new ModuleDataHandler();
+        public static SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader reader;
         public Form1()
         {
             InitializeComponent();
+            connection = new SqlConnection(@"Server=DESKTOP-6OVGGB7\SQLEXPRESS; Initial Catalog =Students; Integrated Security =SSPI");
         }
 
         private void groupBox2_Enter(object sender, EventArgs e)
@@ -25,6 +31,21 @@ namespace PRG281_Project_Group11
         private void groupBox5_Enter(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnModuleAdd_Click(object sender, EventArgs e)
+        {
+            if (txtModName.Text == "" || txtModDesc.Text == "" || txtModLink.Text == "")
+            {
+                MessageBox.Show("Please make sure that all fields are filled out");
+            }
+            else
+            {
+                moduleDataHandler.SaveModule(txtModName.Text, txtModDesc.Text, txtModLink.Text);
+                txtModName.Clear();
+                txtModDesc.Clear();
+                txtModLink.Clear();
+            }
         }
     }
 }
---
+++ ModuleDataHandler.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG281_Project_Group11
+{
+    internal class ModuleDataHandler
+    {
+        private int moduleCode;
+        private string moduleName, moduleDescription, moduleLink;
+
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        private SqlDataReader reader;
+
+        private string connection = @"Server=DESKTOP-6OVGGB7\SQLEXPRESS; Initial Catalog =Students; Integrated Security =SSPI";
+
+        public ModuleDataHandler(int moduleCode, string moduleName, string moduleDescription, string moduleLink)
+        {
+            this.moduleCode = moduleCode;
+            this.moduleName = moduleName;
+            this.moduleDescription = moduleDescription;
+            this.moduleLink = moduleLink;
+        }
+
+        public ModuleDataHandler()
+        {
+
+        }
+
+        public void openConnection()
+        {
+            conn = new SqlConnection(connection);
+            conn.Open();
+        }
+
+        public void SaveModule(string name, string description, string link)
+        {
+            string insertQuery = $"Insert into ModuleDetails (ModuleName, ModuleDescription, ModuleLink) Values ('{name}', '{description}', '{link}')";
+            openConnection();
+
+            cmd = new SqlCommand(insertQuery, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Module successfully added");
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show("Something went wrong. Please try again");
+                MessageBox.Show(err.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        public void deleteModule(int code)
+        {
+            string deleteQuery = $"Delete from StudentModules where ModuleCode = {code}; Delete from ModuleDetails where ModuleCode = {code}";
+            openConnection();
+
+            cmd = new SqlCommand(deleteQuery, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show($"The module {code} has been successfully deleted");
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show("Something went wrong. Please try again.");
+                MessageBox.Show(err.ToString());
+            }
+            finally
+            {
+                conn.Close();  
+            }
+        }
+
+
+        public void updateModule(int code, string name, string description, string link)
+        {
+            string updateQuery = $"Update ModuleDetails SET ModuleName = '{name}', ModuleDescription = '{description}', ModuleLink = '{link}'";
+            openConnection();
+
+            cmd = new SqlCommand(updateQuery, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Module successfully updated");
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show("Something went wrong. Please try again.");
+                MessageBox.Show(err.ToString());
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+    }
+}
